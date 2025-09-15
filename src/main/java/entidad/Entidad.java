dba184a278817ad0b6dec67136d11849dfd68eed
fran <francocristaldo2004@gmail.com>
2025-09-15 20:01:22 -0300

    - Se agrego entidad.Entidad

---
+++ src/main/java/entidad/Entidad.java
@@ -0,0 +1,33 @@
+package entidad;
+
+import mundo.Posicion;
+
+/**
+ * Representa un objeto con posicion en el mundo
+ * ejemplos: personajes, cartas, trampas, etc
+ */
+public class Entidad {
+    public Posicion posicion;
+
+    /**
+     * @param posicionInicial No null
+     */
+    public Entidad(Posicion posicionInicial) {
+        setPosicion(posicionInicial);
+    }
+
+    /**
+     * @param posicion No null
+     */
+    public void setPosicion(Posicion posicion) {
+        if (posicion == null) {
+            throw new NullPointerException("'posicion' no debe ser null");
+        }
+
+        this.posicion = posicion;
+    }
+
+    public Posicion getPosicion() {
+        return posicion;
+    }
+}
