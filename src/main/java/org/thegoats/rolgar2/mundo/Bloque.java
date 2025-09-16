1fa47a86364ba2c8118871b1f2a3e9750730fe2b
Franco Cristaldo <71131695+FranCris2004@users.noreply.github.com>
2025-09-16 17:37:40 -0300

    Update Bloque.java

--- src/main/java/org/thegoats/rolgar2/mundo/Bloque.java
+++ src/main/java/org/thegoats/rolgar2/mundo/Bloque.java
@@ -2,57 +2,28 @@ package org.thegoats.rolgar2.mundo;
 
 public class Bloque {
 	private Posicion posicion;
-	private int fallDamage;
 	private boolean isWalkable;
 	
 	/**
-	 * @param posicion No null
-	 * @param fallDamage No negativo, danio al caer en este bloque
-	 * @param isWalkable Si se puede caminar sobre este bloque
+	 * @param posicion No null, posicion fija del bloque
+	 * @param isWalkable Si se puede caminar o no sobre este bloque
 	 */
-	public Bloque(Posicion posicion, int fallDamage, boolean isWalkable) {
-		setPosicion(posicion);
-		setFallDamage(fallDamage);
-		setIsWalkable(isWalkable);
-	}
-
-	/**
- 	 * @param posicion No null
-     */
-	public void setPosicion(Posicion posicion) {
+	public Bloque(Posicion posicion, boolean isWalkable) {
 		if (posicion == null) {
 			throw new NullPointerException("La posicion debe ser no nula");
 		}
+	
 		this.posicion = posicion;
-	}
-	
-	/**
-	 * @param fallDamage No negativo, danio al caer en este bloque
-	 */
-	public void setFallDamage(int fallDamage) {
-		if (fallDamage < 0) {
-			throw new IllegalArgumentException("El daño de caida debe ser mayor a cero");
-		}
-		this.fallDamage = fallDamage;
-	}
-	
-	/**
-	 * @param isWalkable Si se puede caminar sobre este bloque
-	 */
-	public void setIsWalkable(boolean isWalkable) {
 		this.isWalkable = isWalkable;
 	}
 	
 	/**
-	 * @return fallDamage No negativo
+     * @return posicion No null
 	 */
-	public int getFallDamage() {
-		return fallDamage;
+	public Posicion getPosicion() {
+		return new Posicion(posicion.fila, posicion.columna, posicion.capa);
 	}
 	
-	/**
-	 * @return isWalkable
-	 */
 	public boolean getIsWalkable() {
 		return isWalkable;
 	}
