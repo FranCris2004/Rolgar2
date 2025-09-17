5f11e2808ccb6306d87366c4fd2819bebf1ed22d
Franco Cristaldo <francocristaldo@gmail.com>
2025-09-17 17:08:43 -0300

    Block ahora es un record para garantizar su inmutabilidad, mejorar la seguridad y simplificar el codigo

--- src/main/java/org/thegoats/rolgar2/world/Block.java
+++ src/main/java/org/thegoats/rolgar2/world/Block.java
@@ -1,30 +1,13 @@
 package org.thegoats.rolgar2.world;
 
-public class Block {
-	private Position position;
-	private boolean isWalkable;
-	
+public record Block(Position position, boolean isWalkable) {
 	/**
 	 * @param position No null, posicion fija del bloque
 	 * @param isWalkable Si se puede caminar o no sobre este bloque
 	 */
-	public Block(Position position, boolean isWalkable) {
+	public Block {
 		if (position == null) {
 			throw new NullPointerException("'position' debe ser no nula");
 		}
-	
-		this.position = position;
-		this.isWalkable = isWalkable;
-	}
-	
-	/**
-     * @return position No null
-	 */
-	public Position getPosition() {
-		return position;
-	}
-	
-	public boolean getIsWalkable() {
-		return isWalkable;
 	}
 }
