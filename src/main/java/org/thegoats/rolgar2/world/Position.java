2700d9f4857bbb638c2bb0b0ce528e4edb142e12
Franco Cristaldo <francocristaldo@gmail.com>
2025-09-17 16:57:25 -0300

    Se convirtio a record Position para garantizar su inmutabilidad y asi mejorar la seguridad

--- src/main/java/org/thegoats/rolgar2/world/Position.java
+++ src/main/java/org/thegoats/rolgar2/world/Position.java
@@ -3,71 +3,42 @@ package org.thegoats.rolgar2.world;
 /**
  * Representa una posicion en el mundo
  */
-public class Position {
-    private int row, column, layer;
-
-    /**
-     * Equivalente a Posicion(0, 0, 0)
-     */
-    public Position() {
-        this.row = 0;
-        this.column = 0;
-        this.layer = 0;
-    }
-
+public record Position(int row, int column, int layer) {
     /**
      * @param row No negativo
      * @param column No negativo
      * @param layer No negativo
      */
-    public Position(int row, int column, int layer) {
-        setRow(row);
-        setColumn(column);
-        setLayer(layer);
+    public Position {
+        validateRow(row);
+        validateColumn(column);
+        validateLayer(layer);
     }
 
     /**
      * @param row No negativo
      */
-    public void setRow(int row) {
+    private static void validateRow(int row) {
         if (row < 0) {
             throw new IllegalArgumentException("'row' debe ser positivo o cero");
         }
-
-        this.row = row;
     }
 
     /**
      * @param column No negativo
      */
-    public void setColumn(int column) {
-        if (row < 0) {
+    private static void validateColumn(int column) {
+        if (column < 0) {
             throw new IllegalArgumentException("'column' debe ser positivo o cero");
         }
-
-        this.column = column;
     }
 
     /**
      * @param layer No negativo
      */
-    public void setLayer(int layer) {
-        if (row < 0) {
+    public static void validateLayer(int layer) {
+        if (layer < 0) {
             throw new IllegalArgumentException("'layer' debe ser positivo o cero");
         }
-
-        this.layer = layer;
-    }
-
-    public int getRow() {
-        return row;
-    }
-
-    public int getColumn() {
-        return column;
-    }
-
-    public int getLayer() {
-        return layer;
     }
 }
