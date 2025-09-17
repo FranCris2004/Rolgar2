934a748f954fc8a84f5b7fadb06a1ef8cfaee452
Franco Cristaldo <francocristaldo@gmail.com>
2025-09-17 18:09:49 -0300

    Se implemento util.Assert a world.Block

--- src/main/java/org/thegoats/rolgar2/world/Block.java
+++ src/main/java/org/thegoats/rolgar2/world/Block.java
@@ -1,13 +1,13 @@
 package org.thegoats.rolgar2.world;
 
+import org.thegoats.rolgar2.util.Assert;
+
 public record Block(Position position, boolean isWalkable) {
 	/**
-	 * @param position No null, posicion fija del bloque
+	 * @param position No null, posicion bloque
 	 * @param isWalkable Si se puede caminar o no sobre este bloque
 	 */
 	public Block {
-		if (position == null) {
-			throw new NullPointerException("'position' debe ser no nula");
-		}
+		Assert.notNull(position, "'position' debe ser no nula");
 	}
 }
