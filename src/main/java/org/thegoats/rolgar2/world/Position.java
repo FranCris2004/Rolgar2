45a71cd37f6cd3cedba0253cf0820be3a577fa51
Franco Cristaldo <francocristaldo@gmail.com>
2025-09-17 18:05:55 -0300

    Se implemento Assert en world.Position

--- src/main/java/org/thegoats/rolgar2/world/Position.java
+++ src/main/java/org/thegoats/rolgar2/world/Position.java
@@ -1,5 +1,7 @@
 package org.thegoats.rolgar2.world;
 
+import org.thegoats.rolgar2.util.Assert;
+
 /**
  * Representa una posicion en el mundo
  * @param row No negativo
@@ -8,35 +10,8 @@ package org.thegoats.rolgar2.world;
  */
 public record Position(int row, int column, int layer) {
     public Position {
-        validateRow(row);
-        validateColumn(column);
-        validateLayer(layer);
-    }
-
-    /**
-     * @param row No negativo
-     */
-    private static void validateRow(int row) {
-        if (row < 0) {
-            throw new IllegalArgumentException("'row' debe ser positivo o cero");
-        }
-    }
-
-    /**
-     * @param column No negativo
-     */
-    private static void validateColumn(int column) {
-        if (column < 0) {
-            throw new IllegalArgumentException("'column' debe ser positivo o cero");
-        }
-    }
-
-    /**
-     * @param layer No negativo
-     */
-    public static void validateLayer(int layer) {
-        if (layer < 0) {
-            throw new IllegalArgumentException("'layer' debe ser positivo o cero");
-        }
+        Assert.nonNegative(row, "'row' debe ser positivo o cero");
+        Assert.nonNegative(column, "'column' debe ser positivo o cero");
+        Assert.nonNegative(layer, "'layer' debe ser positivo o cero");
     }
 }
