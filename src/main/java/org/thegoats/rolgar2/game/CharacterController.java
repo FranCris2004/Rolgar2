085b7835e40647777f473a31d62fff8e755d0c14
Franco Cristaldo <francocristaldo@gmail.com>
2025-10-07 19:03:09 -0300

    Se elimino CharacterController

--- src/main/java/org/thegoats/rolgar2/game/CharacterController.java
+++
@@ -1,7 +0,0 @@
-package org.thegoats.rolgar2.game;
-
-import org.thegoats.rolgar2.character.Character;
-
-public interface CharacterController {
-    void DoTurn(Character character);
-}
--- src/main/java/org/thegoats/rolgar2/game/GameCharacter.java
+++ src/main/java/org/thegoats/rolgar2/game/GameCharacter.java
@@ -4,6 +4,5 @@ import org.thegoats.rolgar2.world.Position;
 
 public class GameCharacter {
     public Character character;
-    public CharacterController controller;
     public Position position;
 }
