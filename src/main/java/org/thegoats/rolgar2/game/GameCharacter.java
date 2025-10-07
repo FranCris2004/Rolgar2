1b5bd8de57c88987f03b55391b3d2e731bc6d1ed
Franco Cristaldo <francocristaldo@gmail.com>
2025-10-07 19:04:28 -0300

    Se deselimino CharacterController

--- src/main/java/org/thegoats/rolgar2/game/GameCharacter.java
+++ src/main/java/org/thegoats/rolgar2/game/GameCharacter.java
@@ -4,5 +4,6 @@ import org.thegoats.rolgar2.world.Position;
 
 public class GameCharacter {
     public Character character;
+    public CharacterController controller;
     public Position position;
 }
