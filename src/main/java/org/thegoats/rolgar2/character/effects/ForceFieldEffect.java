46bd96bb9879589203cf870c7609f33e69aee9ba
Franco Cristaldo <francocristaldo@gmail.com>
2025-10-08 12:46:21 -0300

    Se implemento el StatusEffect ForceFieldEffect

--- src/main/java/org/thegoats/rolgar2/character/effects/ForceFieldEffect.java
+++ src/main/java/org/thegoats/rolgar2/character/effects/ForceFieldEffect.java
@@ -4,17 +4,34 @@ import org.thegoats.rolgar2.character.CharacterData;
 import org.thegoats.rolgar2.character.StatusEffect;
 
 public class ForceFieldEffect extends StatusEffect {
-    public ForceFieldEffect(int remainingTurns) {
+    private final float incomingDamageFactorModifier;
+    
+    public ForceFieldEffect(int remainingTurns, float takeDamageFactorModifier) {
         super(remainingTurns);
+
+        if (takeDamageFactorModifier < 0) {
+            throw new IllegalArgumentException("takeDamageFactorModifier no debe ser negativo");
+        }
+        
+        this.incomingDamageFactorModifier = takeDamageFactorModifier;
     }
 
     @Override
     public void onApply(CharacterData character) {
-
+        character.setIncomingDamageFactor(incomingDamageFactorModifier);
     }
 
     @Override
     public void onRemove(CharacterData character) {
+        character.setIncomingDamageFactor(incomingDamageFactorModifier);
+    }
 
+    @Override
+    public String toString() {
+        return String.format(
+                "ForceFieldEffect[remainingTurns=%d, incomingDamageFactor=%.2f]",
+                getRemainingTurns(),
+                incomingDamageFactorModifier
+        );
     }
 }
