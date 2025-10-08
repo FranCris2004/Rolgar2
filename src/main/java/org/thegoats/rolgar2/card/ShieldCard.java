3595e76b520615d076d9cfffa9d20284846acff6
Franco Cristaldo <francocristaldo@gmail.com>
2025-10-08 19:05:36 -0300

    Se implemento ShieldCard

--- src/main/java/org/thegoats/rolgar2/card/ShieldCard.java
+++ src/main/java/org/thegoats/rolgar2/card/ShieldCard.java
@@ -1,10 +1,20 @@
 package org.thegoats.rolgar2.card;
 
+import org.thegoats.rolgar2.character.CharacterData;
+import org.thegoats.rolgar2.character.effects.ForceFieldEffect;
+import org.thegoats.rolgar2.util.Assert;
+
 public class ShieldCard implements Card{
+    private CharacterData target = null;
+
+    public void setTarget(CharacterData target) {
+        this.target = target;
+    }
 
     @Override
     public void use() {
-
+        Assert.notNull(target, "El target no ha sido setteado.");
+        target.applyEffect(new ForceFieldEffect(1, target.getIncomingDamageFactor()));
     }
 }
 
