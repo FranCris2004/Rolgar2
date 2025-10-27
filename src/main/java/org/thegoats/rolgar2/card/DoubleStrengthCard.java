8fe450d06f72570b2619e5738c6a53f54fa733a6
nahuel <nahuelestudio21@gmail.com>
2025-10-27 18:04:29 -0300

    Se implemento DoubleStrengthCard

--- src/main/java/org/thegoats/rolgar2/card/DoubleStrengthCard.java
+++ src/main/java/org/thegoats/rolgar2/card/DoubleStrengthCard.java
@@ -1,10 +1,24 @@
 package org.thegoats.rolgar2.card;
 
+import org.thegoats.rolgar2.character.CharacterData;
+import org.thegoats.rolgar2.character.effects.DoubleStrengthEffect;
+import org.thegoats.rolgar2.util.Assert;
+
 public class DoubleStrengthCard implements Card{
+    private CharacterData target = null;
+
+    /**
+     *
+     * @param target objetivo a setear
+     */
+    public void setTarget(CharacterData target) {
+        this.target = target;
+    }
 
     @Override
     public void use() {
-
+        Assert.notNull(target, "El target no ha sido setteado.");
+        target.applyEffect(new DoubleStrengthEffect(1));
     }
 }
 
