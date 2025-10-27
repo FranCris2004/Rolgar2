962ae09f1d1a9ebb76d9e319f50e3c748f005f1c
nahuel <nahuelestudio21@gmail.com>
2025-10-27 17:18:49 -0300

    Se agrego onApply, onRemove y constructor de DoubleStrengthEffect

---
+++ src/main/java/org/thegoats/rolgar2/character/effects/DoubleStrengthEffect.java
@@ -0,0 +1,24 @@
+package org.thegoats.rolgar2.character.effects;
+
+import org.thegoats.rolgar2.character.CharacterData;
+import org.thegoats.rolgar2.character.StatusEffect;
+
+public class DoubleStrengthEffect extends StatusEffect {
+
+    public DoubleStrengthEffect(int remainingTurns){
+        super(remainingTurns);
+
+    }
+
+    @Override
+    public void onApply(CharacterData character) {
+        character.setStrength(character.getStrength()*2);
+    }
+
+    @Override
+    public void onRemove(CharacterData character) {
+        character.setStrength(character.getStrength()/2);
+    }
+
+
+}
