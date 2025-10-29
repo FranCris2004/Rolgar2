38dd9ed23c4eb96ed3790615accde64a590d3964
Franco Cristaldo <francocristaldo@gmail.com>
2025-10-29 20:07:23 -0300

    Se implemento HealingCard

--- src/main/java/org/thegoats/rolgar2/card/HealingCard.java
+++ src/main/java/org/thegoats/rolgar2/card/HealingCard.java
@@ -1,10 +1,19 @@
 package org.thegoats.rolgar2.card;
 
-public class HealingCard implements Card{
+import org.thegoats.rolgar2.util.Assert;
+
+public class HealingCard extends CardWithCharacterTarget {
+    private int healingPoints = -1;
 
     @Override
     public void use() {
+        validateSetTarget();
+        Assert.positive(healingPoints, "healingPoints no fue setteado");
+        getTarget().recoverHealth(healingPoints);
+    }
 
+    public void setHealingPoints(int healingPoints) {
+        Assert.positive(healingPoints, "healingPoints debe ser positiva.");
+        this.healingPoints = healingPoints;
     }
 }
-
