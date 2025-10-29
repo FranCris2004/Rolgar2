4ff33c45b1c8fef7860a65285896bf01d4c65c17
Franco Cristaldo <francocristaldo@gmail.com>
2025-10-29 20:07:55 -0300

    Se implemento StrengthCard

---
+++ src/main/java/org/thegoats/rolgar2/card/StrengthCard.java
@@ -0,0 +1,12 @@
+package org.thegoats.rolgar2.card;
+
+import org.thegoats.rolgar2.character.effects.DoubleStrengthEffect;
+
+public class StrengthCard extends CardWithStatusEffect {
+    @Override
+    public void use() {
+        validateSetTarget();
+        validateSetRemainingTurns();
+        getTarget().applyEffect(new DoubleStrengthEffect(getTarget(), getRemainingTurns()));
+    }
+}
