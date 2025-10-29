e4682b32026e3445c281be0419d278098a9a560a
LucasCascini <lcascini@fi.uba.ar>
2025-10-29 18:03:02 -0300

    - Se agrego FreezeEffect

--- src/main/java/org/thegoats/rolgar2/character/effects/FreezeEffect.java
+++ src/main/java/org/thegoats/rolgar2/character/effects/FreezeEffect.java
@@ -2,21 +2,37 @@ package org.thegoats.rolgar2.character.effects;
 
 import org.thegoats.rolgar2.character.CharacterData;
 import org.thegoats.rolgar2.character.StatusEffect;
+import org.thegoats.rolgar2.util.Assert;
+
+/**
+ * Efecto de congelamiento aplicado por la carta IceBall
+ */
 
 public class FreezeEffect extends StatusEffect {
-
-
-    public FreezeEffect(int remainingTurns){
-        super(remainingTurns);
+    private int moves;
+    /**
+     * Construye el efecto de congelamiento con los turnos indicados
+     * @param remainingTurns mayor a cero
+     */
+    public FreezeEffect(CharacterData character, int remainingTurns){
+        super(character, remainingTurns);
     }
 
+    /**
+     * Guarda en un atributo auxiliar los movimientos por turno anteriores al congelamiento
+     */
     @Override
-    public void onApply(CharacterData character) {
-
+    public void onApply() {
+        this.moves = getCharacter().getMoves();
+        getCharacter().setMoves(0);
     }
 
+    /**
+     * Devuelve sus movimientos por turno al personaje
+     */
     @Override
-    public void onRemove(CharacterData character) {
-
+    public void onRemove() {
+        Assert.notNull(getCharacter(), "CharacterData no puede ser null");
+        getCharacter().setMoves(moves);
     }
 }
