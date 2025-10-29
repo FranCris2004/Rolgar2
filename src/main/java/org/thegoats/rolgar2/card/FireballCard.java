fe8c77643482758cc89ddd6bf6c4b59a5aefaf5f
Franco Cristaldo <francocristaldo@gmail.com>
2025-10-29 20:07:07 -0300

    Se implemento FireballCard

--- src/main/java/org/thegoats/rolgar2/card/FireballCard.java
+++ src/main/java/org/thegoats/rolgar2/card/FireballCard.java
@@ -1,10 +1,19 @@
 package org.thegoats.rolgar2.card;
 
-public class FireballCard implements Card{
+import org.thegoats.rolgar2.util.Assert;
+
+public class FireballCard extends CardWithCharacterTarget {
+    private int damage = -1;
 
     @Override
     public void use() {
+        validateSetTarget();
+        Assert.positive(damage, "damage no ha sido setteado.");
+        getTarget().takeDamage(damage);
+    }
 
+    public void setDamage(int damage) {
+        Assert.positive(damage, "damage debe ser positivo.");
+        this.damage = damage;
     }
 }
-
