5b926f797e1b4122b6eadb534ef819ba9b3a5e6e
Franco Cristaldo <francocristaldo@gmail.com>
2025-10-29 20:08:23 -0300

    Se agrego CardWithCharacterTarget y CardWithStatusEffect para facilitar la implementacion de cartas

---
+++ src/main/java/org/thegoats/rolgar2/card/CardWithCharacterTarget.java
@@ -0,0 +1,21 @@
+package org.thegoats.rolgar2.card;
+
+import org.thegoats.rolgar2.character.CharacterData;
+import org.thegoats.rolgar2.util.Assert;
+
+public abstract class CardWithCharacterTarget implements Card {
+    private CharacterData target = null;
+
+    public void setTarget(CharacterData target) {
+        Assert.notNull(target, "El target no debe ser nulo");
+        this.target = target;
+    }
+
+    public CharacterData getTarget() {
+        return target;
+    }
+
+    public void validateSetTarget() {
+        Assert.notNull(target, "El target no ha sido setteado");
+    }
+}
---
+++ src/main/java/org/thegoats/rolgar2/card/CardWithStatusEffect.java
@@ -0,0 +1,20 @@
+package org.thegoats.rolgar2.card;
+
+import org.thegoats.rolgar2.util.Assert;
+
+public abstract class CardWithStatusEffect extends CardWithCharacterTarget {
+    private int remainingTurns = -1;
+
+    public void setRemainingTurns(int remainingTurns) {
+        Assert.positive(remainingTurns, "remainingTurns debe ser positivo.");
+        this.remainingTurns = remainingTurns;
+    }
+
+    public int getRemainingTurns() {
+        return remainingTurns;
+    }
+
+    public void validateSetRemainingTurns() {
+        Assert.positive(remainingTurns, "remainingTurns no ha sido setteado.");
+    }
+}
