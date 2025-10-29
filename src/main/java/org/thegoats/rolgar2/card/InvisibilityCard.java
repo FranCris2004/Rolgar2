f96eb1f413d5001c561df880a450bd90e1e66b1a
Franco Cristaldo <francocristaldo@gmail.com>
2025-10-29 20:07:40 -0300

    Se implemento InvisibilityCard

--- src/main/java/org/thegoats/rolgar2/card/InvisibilityCard.java
+++ src/main/java/org/thegoats/rolgar2/card/InvisibilityCard.java
@@ -1,26 +1,15 @@
 package org.thegoats.rolgar2.card;
 
-import org.thegoats.rolgar2.character.CharacterData;
 import org.thegoats.rolgar2.character.effects.InvisibilityEffect;
-import org.thegoats.rolgar2.util.Assert;
 
-public class InvisibilityCard implements Card {
-    private CharacterData target = null;
-
-    /**
-     * @param target dado un CharacterData o null, lo setea
-     */
-    public void setTarget(CharacterData target) {
-        Assert.notNull(target, "El target no debe ser nulo.");
-        this.target = target;
-    }
-
+public class InvisibilityCard extends CardWithStatusEffect {
     /**
      * Aplica el efecto de la carta sobre el personaje destino, no puede ser nulo llegado este punto
      */
     @Override
     public void use() {
-        Assert.notNull(target, "El target no ha sido setteado.");
-        target.applyEffect(new InvisibilityEffect(target, 1));
+        validateSetTarget();
+        validateSetRemainingTurns();
+        getTarget().applyEffect(new InvisibilityEffect(getTarget(), getRemainingTurns()));
     }
 }
