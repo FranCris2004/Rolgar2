41b16e44a1096a62ad850764247396da32046366
Franco Cristaldo <francocristaldo@gmail.com>
2025-10-29 20:07:33 -0300

    Se implemento IceballCard

--- src/main/java/org/thegoats/rolgar2/card/IceballCard.java
+++ src/main/java/org/thegoats/rolgar2/card/IceballCard.java
@@ -1,26 +1,15 @@
 package org.thegoats.rolgar2.card;
 
-import org.thegoats.rolgar2.character.CharacterData;
 import org.thegoats.rolgar2.character.effects.FreezeEffect;
-import org.thegoats.rolgar2.util.Assert;
 
-public class IceballCard implements Card {
-    CharacterData target = null;
-    int effectDuration = 1;
-
-    public void setEffectDuration(int duration) {
-        Assert.positive(duration, "Los turnos no pueden ser negativos ni cero, se ingreso "+duration);
-        effectDuration = duration;
-    }
-
-    public void setTarget(CharacterData target) {
-        this.target = target;
-    }
-
+public class IceballCard extends CardWithStatusEffect {
+    /**
+     * Aplica el efecto de la carta sobre el personaje destino, no puede ser nulo llegado este punto
+     */
     @Override
     public void use() {
-        if (target != null) {
-            target.applyEffect(new FreezeEffect(target, effectDuration));
-        }
+        validateSetTarget();
+        validateSetRemainingTurns();
+        getTarget().applyEffect(new FreezeEffect(getTarget(), getRemainingTurns()));
     }
 }
