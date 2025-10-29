5f722cecdc6b9b90c4c641b088cf19b1a0ee9b6f
Franco Cristaldo <francocristaldo@gmail.com>
2025-10-29 20:06:58 -0300

    Se implemento DoubleMoveCard

--- src/main/java/org/thegoats/rolgar2/card/DoubleMoveCard.java
+++ src/main/java/org/thegoats/rolgar2/card/DoubleMoveCard.java
@@ -1,10 +1,12 @@
 package org.thegoats.rolgar2.card;
 
-public class DoubleMoveCard implements Card{
+import org.thegoats.rolgar2.character.effects.DoubleStrengthEffect;
 
+public class DoubleMoveCard extends CardWithStatusEffect {
     @Override
     public void use() {
-
+        validateSetTarget();
+        validateSetRemainingTurns();
+        getTarget().applyEffect(new DoubleStrengthEffect(getTarget(), getRemainingTurns()));
     }
 }
-
