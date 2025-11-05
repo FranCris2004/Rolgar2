e5a92b2431880b274d3799fcc0e56312dc28146d
LucasCascini <lcascini@fi.uba.ar>
2025-11-05 18:17:14 -0300

    Se agregaron comentarios a CardWithCharacterTarget y CardWithStatusEffect

--- src/main/java/org/thegoats/rolgar2/card/CardWithCharacterTarget.java
+++ src/main/java/org/thegoats/rolgar2/card/CardWithCharacterTarget.java
@@ -3,6 +3,10 @@ package org.thegoats.rolgar2.card;
 import org.thegoats.rolgar2.character.CharacterData;
 import org.thegoats.rolgar2.util.Assert;
 
+/**
+ * La mayoria de las cartas tienen un target que es de tipo CharacterData, por lo tanto abstraemos los metodos
+ * getTarget, validateTarget, setTarget y el atributo target en esta clase abstracta.
+ */
 public abstract class CardWithCharacterTarget implements Card {
     private CharacterData target = null;
 
--- src/main/java/org/thegoats/rolgar2/card/CardWithStatusEffect.java
+++ src/main/java/org/thegoats/rolgar2/card/CardWithStatusEffect.java
@@ -2,18 +2,35 @@ package org.thegoats.rolgar2.card;
 
 import org.thegoats.rolgar2.util.Assert;
 
+/**
+ * Muchas de las cartas que tienen un CharacterData como target, le aplican un efecto de estado StatusEffect a dicho personaje,
+ * por lo tanto abstraemos en esta clase el atributo remainingTurns, que sera la duracion del efecto de estado en turnos,
+ * y los metodos setRemainingTurns, getRemainingTurns, y validateRemainingTurns. Todas las cartas que apliquen un efecto
+ * de estado heredaran de esta clase.
+ */
 public abstract class CardWithStatusEffect extends CardWithCharacterTarget {
     private Integer remainingTurns = null;
 
+    /**
+     * Recibe una cantidad de turnos restantes 'remainingTurns', y si son mayores a 0 los setea.
+     * @param remainingTurns mayor a cero, turnos que durara el efecto de estado de la carta al ser aplicado sobre un personaje
+     */
     public void setRemainingTurns(int remainingTurns) {
         Assert.positive(remainingTurns, "remainingTurns debe ser positivo.");
         this.remainingTurns = remainingTurns;
     }
 
+    /**
+     * Devuelve los turnos que la carta durara al usarse sobre un personaje
+     * @return turnos que durara la carta
+     */
     public int getRemainingTurns() {
         return remainingTurns;
     }
 
+    /**
+     * Chequea que los turnos ya hayan sido correctamente setteados y no sigan siendo null.
+     */
     public void validateRemainingTurns() {
         Assert.notNull(remainingTurns, "remainingTurns no ha sido setteado.");
     }
