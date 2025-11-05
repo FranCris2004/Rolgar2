6f3e1ced65c0ecf6597e7b53f6016d1c7555cb38
Franco Cristaldo <francocristaldo@gmail.com>
2025-11-05 17:06:55 -0300

    Se hizo abstract class a CharacterController

--- src/main/java/org/thegoats/rolgar2/game/CharacterController.java
+++ src/main/java/org/thegoats/rolgar2/game/CharacterController.java
@@ -1,11 +1,23 @@
 package org.thegoats.rolgar2.game;
 
 import org.thegoats.rolgar2.character.CharacterData;
+import org.thegoats.rolgar2.util.Assert;
 
 /**
  * Interfaz de las clases CharacterController,
  * las cuales son las que contienen la logica de control del personaje y si es necesario, toman el input del usuario
  */
-public interface CharacterController {
-    void doTurn(CharacterData character);
+public abstract class CharacterController {
+    private final CharacterData characterData;
+
+    public CharacterController(CharacterData characterData) {
+        Assert.notNull(characterData, "CharacterData no debe ser nulo");
+        this.characterData = characterData;
+    }
+
+    protected CharacterData getCharacterData() {
+        return characterData;
+    }
+
+    public abstract void doTurn();
 }
