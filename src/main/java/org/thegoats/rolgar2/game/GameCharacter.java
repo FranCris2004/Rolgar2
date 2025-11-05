4782597fab457acb37d1f1a8489ecf6ed99f0ffa
Franco Cristaldo <francocristaldo@gmail.com>
2025-11-05 17:32:08 -0300

    Se implemento GameCharacter completamente

--- src/main/java/org/thegoats/rolgar2/game/GameCharacter.java
+++ src/main/java/org/thegoats/rolgar2/game/GameCharacter.java
@@ -1,9 +1,27 @@
 package org.thegoats.rolgar2.game;
 
 import org.thegoats.rolgar2.character.CharacterData;
+import org.thegoats.rolgar2.util.Assert;
 import org.thegoats.rolgar2.world.Position;
 
 public final class GameCharacter {
-    public CharacterData characterData;
-    public Position position;
+    public final boolean isPlayer;
+    public final CharacterData characterData;
+    private Position position;
+
+    public GameCharacter(CharacterData characterData, Position position, boolean isPlayer) {
+        Assert.notNull(characterData, "characterData no puede ser nulo");
+        this.characterData = characterData;
+        this.isPlayer = isPlayer;
+        setPosition(position);
+    }
+
+    public void setPosition(Position position) {
+        Assert.notNull(position, "position no puede ser nulo.");
+        this.position = position;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
 }
