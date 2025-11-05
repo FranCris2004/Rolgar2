930d4e22e4a055aaf2345f17ca54a0634cfbfc8f
LucasCascini <lcascini@fi.uba.ar>
2025-11-05 18:29:55 -0300

    Se agregaron comentarios a DoubleMoveCard

--- src/main/java/org/thegoats/rolgar2/card/DoubleMoveCard.java
+++ src/main/java/org/thegoats/rolgar2/card/DoubleMoveCard.java
@@ -2,11 +2,29 @@ package org.thegoats.rolgar2.card;
 
 import org.thegoats.rolgar2.character.effects.DoubleMoveEffect;
 
+/**
+ * Carta de doble movimiento. Antes de ser usada se le debe settear un target y una duration. Duration debe ser extraida
+ * del correspondiente config.json
+ */
 public class DoubleMoveCard extends CardWithStatusEffect {
+
+    /**
+     * Chequea que la carta ya tenga asignados target y duration, y de ser asi, le aplica al target el efecto de estado
+     * DoubleMoveEffect
+     */
     @Override
     public void use() {
         validateTarget();
         validateRemainingTurns();
         getTarget().applyEffect(new DoubleMoveEffect(getTarget(), getRemainingTurns()));
+    }
+
+    /**
+     * Devuelve la carta en formato string
+     * @return formato en String de la carta, con el formato NombreDeCarta[atributo1=valor1, atributo2=valor2, ..., atributoN=valorN
+     */
+    @Override
+    public String toString(){
+        return String.format("DoubleMoveCard[target=%s, Duration=%d]", getTarget().toString(), getRemainingTurns());
     }
 }
