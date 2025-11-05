965cf02eccb859e6a702ac58a30dfc46adba09af
LucasCascini <lcascini@fi.uba.ar>
2025-11-05 17:49:24 -0300

    Se agregaron comentarios a CardWithCharacterTarget

--- src/main/java/org/thegoats/rolgar2/card/CardWithCharacterTarget.java
+++ src/main/java/org/thegoats/rolgar2/card/CardWithCharacterTarget.java
@@ -6,15 +6,25 @@ import org.thegoats.rolgar2.util.Assert;
 public abstract class CardWithCharacterTarget implements Card {
     private CharacterData target = null;
 
+    /**
+     * Dado un CharacterData, lo fija como el objetivo de la carta
+     * @param target no null, objetivo de la carta
+     */
     public void setTarget(CharacterData target) {
         Assert.notNull(target, "El target no debe ser nulo");
         this.target = target;
     }
 
+    /**
+     * @return el objetivo de la carta
+     */
     public CharacterData getTarget() {
         return target;
     }
 
+    /**
+     * Chequea que el actual objetivo de la carta ya haya sido asignado a un CharacterData.
+     */
     public void validateTarget() {
         Assert.notNull(target, "El target no ha sido setteado");
     }
