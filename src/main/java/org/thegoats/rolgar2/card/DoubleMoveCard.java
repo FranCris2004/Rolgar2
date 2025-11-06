48294cf24a0b83da4d697dead0d8439cd846b603
Franco Cristaldo <francocristaldo@gmail.com>
2025-11-06 20:59:07 -0300

    Se implemento DoubleMoveCard.Factory

--- src/main/java/org/thegoats/rolgar2/card/DoubleMoveCard.java
+++ src/main/java/org/thegoats/rolgar2/card/DoubleMoveCard.java
@@ -9,4 +9,11 @@ public class DoubleMoveCard extends CardWithStatusEffect {
         validateRemainingTurns();
         getTarget().applyEffect(new DoubleMoveEffect(getTarget(), getRemainingTurns()));
     }
+
+    public static class Factory implements CardFactory<DoubleMoveCard> {
+        @Override
+        public DoubleMoveCard create() {
+            return new DoubleMoveCard();
+        }
+    }
 }
