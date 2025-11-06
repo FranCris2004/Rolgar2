fbd71decdb36fc1fa4934c63832ed80de0904b8f
Franco Cristaldo <francocristaldo@gmail.com>
2025-11-06 18:17:25 -0300

    Se renombro ConsoleLogger a ConsoleLoger

--- src/main/java/org/thegoats/rolgar2/util/ConsoleLogger.java
+++ src/main/java/org/thegoats/rolgar2/util/ConsoleLogger.java
@@ -1,6 +1,6 @@
 package org.thegoats.rolgar2.util;
 
-public final class ConsoleLoger implements Logger {
+public final class ConsoleLogger implements Logger {
     @Override
     public void log(String message) {
         System.out.println(message);
