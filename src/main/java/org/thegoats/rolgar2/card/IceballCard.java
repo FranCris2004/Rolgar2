9587a61d2a32e519895e4698296125153125837b
LucasCascini <lcascini@fi.uba.ar>
2025-11-06 12:07:51 -0300

    Se agregaron comentarios y toString() a IceballCard

--- src/main/java/org/thegoats/rolgar2/card/IceballCard.java
+++ src/main/java/org/thegoats/rolgar2/card/IceballCard.java
@@ -2,14 +2,28 @@ package org.thegoats.rolgar2.card;
 
 import org.thegoats.rolgar2.character.effects.FreezeEffect;
 
+/**
+ * Carta que aplica un efecto de estado de congelamiento al 'target' indicado con 'duration' turnos.
+ */
 public class IceballCard extends CardWithStatusEffect {
     /**
-     * Aplica el efecto de la carta sobre el personaje destino, no puede ser nulo llegado este punto
+     * Aplica el efecto de la carta sobre el 'target' indicado, con una duracion de 'duration' turnos
      */
     @Override
     public void use() {
         validateTarget();
         validateRemainingTurns();
         getTarget().applyEffect(new FreezeEffect(getTarget(), getRemainingTurns()));
+    }
+
+    /**
+     * Devuelve una version en formato String de la carta, con el formato
+     * NombreDeCarta[atributo1=valor1, atributo2=valor2, ..., atributoN=valorN]
+     * @return version en formato string de la carta
+     */
+    public String toString(){
+        return String.format("IceballCard[target=%s, duration=%d",
+                getTarget().toString(),
+                getRemainingTurns());
     }
 }
