a2e3cb36dabc5aa73659c9a2ab132ec6073365d3
Franco Cristaldo <francocristaldo@gmail.com>
2025-11-06 18:17:37 -0300

    Se documento la clase Logger

--- src/main/java/org/thegoats/rolgar2/util/Logger.java
+++ src/main/java/org/thegoats/rolgar2/util/Logger.java
@@ -1,5 +1,13 @@
 package org.thegoats.rolgar2.util;
 
+/**
+ * Interfaz que representa un sistema de logs,
+ * donde y como se impriman los mensajes depende de la implementacion
+ */
 public interface Logger {
+    /**
+     * Imprime un mensaje
+     * @param message mensaje a imprimir
+     */
     void log(String message);
 }
