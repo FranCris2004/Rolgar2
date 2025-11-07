895171129ea07fa6b6c7ede75b3351f40c83062c
LucasCascini <lcascini@fi.uba.ar>
2025-11-07 16:32:10 -0300

    Agrego comentarios en InvisibilityCard

--- src/main/java/org/thegoats/rolgar2/card/InvisibilityCard.java
+++ src/main/java/org/thegoats/rolgar2/card/InvisibilityCard.java
@@ -27,17 +27,36 @@ public class InvisibilityCard extends CardWithStatusEffect {
      * NombreDeCarta[atributo1=valor1, atributo2=valor2, ..., atributoN=valorN]
      * @return version en formato string de la carta
      */
+    @Override
     public String toString(){
         return String.format("InvisibilityCard[target=%s, duration=%d]",
-                getTarget().toString(),
+                getTarget(),
                 getDuration());
     }
 
+    /**
+     * Fábrica de cartas de invisibilidad, construye la fábrica de
+     * CardWithStatusEffect con un generador aleatorio, piso y techo de duraciones,
+     * y a partir de ahi permite utilizar el metodo create() para abstraerse de la implementacion
+     * y generar una carta cuyo efecto de estado a aplicar tenga duracion aleatoria
+     */
     public static class Factory extends CardWithStatusEffect.Factory<InvisibilityCard> {
+
+        /**
+         * Construye la fabrica de cartas de invisibilidad
+         * @param random no null, objeto generador aleatorio
+         * @param durationFloor mayor a cero, menor a 'durationRoof'
+         * @param durationRoof mayor a cero, mayor a 'durationFloor'
+         */
         public Factory(Random random, int durationFloor, int durationRoof) {
             super(random, durationFloor, durationRoof);
         }
 
+        /**
+         * Crea una carta de invisibilidad cuyo efecto de estado tiene una duración aleatoria entre
+         * 'durationFloor' y 'durationRoof' pasados por parámetro al constructor de Factory
+         * @return carta de invisibilidad
+         */
         @Override
         public InvisibilityCard create() {
             return new InvisibilityCard(getRandomDuration());
