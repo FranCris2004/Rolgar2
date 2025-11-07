7cd2c6841efee1bb0013b7adff05c7c352dccc04
Franco Cristaldo <francocristaldo@gmail.com>
2025-11-07 16:24:03 -0300

    Se agrego Player a GameCharacter

--- src/main/java/org/thegoats/rolgar2/game/GameCharacter.java
+++ src/main/java/org/thegoats/rolgar2/game/GameCharacter.java
@@ -1,18 +1,19 @@
 package org.thegoats.rolgar2.game;
 
 import org.thegoats.rolgar2.character.CharacterData;
+import org.thegoats.rolgar2.player.Player;
 import org.thegoats.rolgar2.util.Assert;
 import org.thegoats.rolgar2.world.WorldCell;
 
 public final class GameCharacter {
-    public final boolean isPlayer;
+    public final Player player;
     public final CharacterData characterData;
     private WorldCell worldCell;
 
-    public GameCharacter(CharacterData characterData, WorldCell initialWorldCell, boolean isPlayer) {
+    public GameCharacter(CharacterData characterData, WorldCell initialWorldCell, Player player) {
         Assert.notNull(characterData, "characterData no puede ser nulo");
         this.characterData = characterData;
-        this.isPlayer = isPlayer;
+        this.player = player;
         setWorldCell(initialWorldCell);
     }
 
@@ -23,5 +24,9 @@ public final class GameCharacter {
 
     public WorldCell getWorldCell() {
         return worldCell;
+    }
+
+    public boolean isPlayerCharacter() {
+        return player != null;
     }
 }
