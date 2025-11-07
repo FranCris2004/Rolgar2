e618c1b16fa6183348e83fcdf92454095bc7621f
Franco Cristaldo <francocristaldo@gmail.com>
2025-11-06 21:34:42 -0300

    Se agrego InvisibilityCard.Factory

--- src/main/java/org/thegoats/rolgar2/card/InvisibilityCard.java
+++ src/main/java/org/thegoats/rolgar2/card/InvisibilityCard.java
@@ -2,14 +2,30 @@ package org.thegoats.rolgar2.card;
 
 import org.thegoats.rolgar2.character.effects.InvisibilityEffect;
 
+import java.util.Random;
+
 public class InvisibilityCard extends CardWithStatusEffect {
+    public InvisibilityCard(int remainingTurns) {
+        super(remainingTurns);
+    }
+
     /**
      * Aplica el efecto de la carta sobre el personaje destino, no puede ser nulo llegado este punto
      */
     @Override
     public void use() {
         validateTarget();
-        validateRemainingTurns();
         getTarget().applyEffect(new InvisibilityEffect(getTarget(), getRemainingTurns()));
+    }
+
+    public static class Factory extends CardWithStatusEffect.Factory<InvisibilityCard> {
+        public Factory(Random random, int minRemainingTurns, int maxRemainingTurns) {
+            super(random, minRemainingTurns, maxRemainingTurns);
+        }
+
+        @Override
+        public InvisibilityCard create() {
+            return new InvisibilityCard(getRandomRemainingTurns());
+        }
     }
 }
