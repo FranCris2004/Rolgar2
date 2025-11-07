c6044bbb0455832c1c9a058b607fd8b77a6eaeec
Franco Cristaldo <francocristaldo@gmail.com>
2025-11-06 21:03:12 -0300

    Se implemento FireballCard.Factory

--- src/main/java/org/thegoats/rolgar2/card/FireballCard.java
+++ src/main/java/org/thegoats/rolgar2/card/FireballCard.java
@@ -2,18 +2,40 @@ package org.thegoats.rolgar2.card;
 
 import org.thegoats.rolgar2.util.Assert;
 
+import java.util.Random;
+
 public class FireballCard extends CardWithCharacterTarget {
-    private Integer damage = null;
+    private final int damage;
+
+    public FireballCard(int damage) {
+        Assert.positive(damage, "damage debe ser positivo.");
+        this.damage = damage;
+    }
 
     @Override
     public void use() {
         validateTarget();
-        Assert.notNull(damage, "damage no ha sido setteado.");
         getTarget().takeDamage(damage);
     }
 
-    public void setDamage(int damage) {
-        Assert.positive(damage, "damage debe ser positivo.");
-        this.damage = damage;
+    public static class Factory implements CardFactory<FireballCard> {
+        private final Random random;
+        private final int minDamage;
+        private final int maxDamage;
+
+        public Factory(Random random, int minDamage, int maxDamage) {
+            Assert.notNull(random, "random no puede ser nulo.");
+            Assert.positive(minDamage, "minDamage debe ser positivo.");
+            Assert.positive(maxDamage, "maxDamage debe ser positivo.");
+            Assert.isTrue(minDamage <= maxDamage, "minDamage debe ser menor a maxDamage.");
+            this.random = random;
+            this.minDamage = minDamage;
+            this.maxDamage = maxDamage;
+        }
+
+        @Override
+        public FireballCard create() {
+            return new FireballCard(random.nextInt(minDamage, maxDamage + 1));
+        }
     }
 }
