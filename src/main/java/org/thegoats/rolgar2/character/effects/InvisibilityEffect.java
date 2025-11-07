5510490ebaad5f24348825f6cc09fbcf57e70c51
LucasCascini <lcascini@fi.uba.ar>
2025-11-07 17:54:59 -0300

    Agrego comentarios y toString() en InvisibilityEffect

--- src/main/java/org/thegoats/rolgar2/character/effects/InvisibilityEffect.java
+++ src/main/java/org/thegoats/rolgar2/character/effects/InvisibilityEffect.java
@@ -4,27 +4,42 @@ import org.thegoats.rolgar2.character.CharacterData;
 import org.thegoats.rolgar2.character.StatusEffect;
 
 /**
- * Hace al personaje invisible
+ * Efecto de invisibilidad: hace al personaje invisible durante 'duration' turnos
  */
 public class InvisibilityEffect extends StatusEffect {
-    public InvisibilityEffect(CharacterData character, int remainingTurns) {
-        super(character, remainingTurns);
+    /**
+     * Construye el efecto con character como objetivo y duracion 'duration'
+     * @param character no null, personaje al que se le aplicará el efecto
+     * @param duration mayor a cero, duracion en turnos del efecto
+     */
+    public InvisibilityEffect(CharacterData character, int duration) {
+        super(character, duration);
     }
 
+    /**
+     * Aplica el efecto sobre character (ver padre)
+     */
     @Override
     public void onApply() {
         getCharacter().setVisible(false);
     }
 
+    /**
+     * Remueve el efecto de character (ver padre)
+     */
     @Override
     public void onRemove() {
         getCharacter().setVisible(true);
     }
 
+    /**
+     * @return version en formato String del efecto
+     */
     @Override
     public String toString() {
         return String.format(
-                "InvisibilityEffect[remainingTurns=%d]",
+                "InvisibilityEffect[character=%s, remainingTurns=%d]",
+                getCharacter(),
                 getRemainingTurns()
         );
     }
