c4aadd843411126b73487c115b78247572aaa576
Franco Cristaldo <francocristaldo@gmail.com>
2025-11-09 14:16:59 -0300

    Se completo Rolgar.main

--- src/main/java/org/thegoats/rolgar2/Rolgar.java
+++ src/main/java/org/thegoats/rolgar2/Rolgar.java
@@ -2,17 +2,34 @@ package org.thegoats.rolgar2;
 
 import org.thegoats.rolgar2.game.GameBuilder;
 import org.thegoats.rolgar2.util.ConsoleLogger;
+import org.thegoats.rolgar2.util.Options;
 
 public class Rolgar {
     public static void main(String[] args) {
         System.out.println("Rolgar 2");
 
-        GameBuilder gameBuilder = new GameBuilder();
-        gameBuilder
-                .setLogger(new ConsoleLogger())
-                .loadDifficulties("./assets/difficulties/")
-                .loadMaps("./assets/maps/")
-                .build()
-                .run();
+        var playAgainPrompt = new Options(
+                "Jugar de nuevo?",
+                new String[]{"si", "no"},
+                "Opcion invalida, intentelo nuevamente",
+                3,
+                false
+                );
+
+        boolean playAgain = true;
+        while (playAgain) {
+            GameBuilder.createBuilder()
+                    // configura el juego
+                    .setLogger(new ConsoleLogger())
+                    .selectDifficulty("./assets/difficulties/")
+                    .selectMap("./assets/maps/")
+                    .initPlayers()
+                    // construye el juego
+                    .build()
+                    // ejecuta el juego
+                    .run();
+
+            playAgain = playAgainPrompt.choose().orElse("no").equals("si");
+        }
     }
 }
