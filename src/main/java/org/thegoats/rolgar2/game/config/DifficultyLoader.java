ecb78b94e15ecbef8f8dcfa770d096b1880f953d
Franco Cristaldo <francocristaldo@gmail.com>
2025-11-09 13:44:03 -0300

    Se agrego DifficultyLoader

---
+++ src/main/java/org/thegoats/rolgar2/game/config/DifficultyLoader.java
@@ -0,0 +1,9 @@
+package org.thegoats.rolgar2.game.config;
+
+import java.util.Set;
+
+public class DifficultyLoader {
+    public static Set<DifficultyConfig> loadDifficulties(String mapsDirectoryPath) {
+        return null;
+    };
+}
