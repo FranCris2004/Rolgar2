7df3b92989888f1e7edb1d13be2e997165919b8a
Franco Cristaldo <francocristaldo@gmail.com>
2025-11-09 13:44:09 -0300

    Se agrego MapLoader

---
+++ src/main/java/org/thegoats/rolgar2/game/config/MapLoader.java
@@ -0,0 +1,9 @@
+package org.thegoats.rolgar2.game.config;
+
+import java.util.Set;
+
+public class MapLoader {
+    public static Set<MapConfig> loadMaps(String mapsDirectoryPath) {
+        return null;
+    };
+}
