d1f03182588b7722429a069514ef343caededf11
Franco Cristaldo <francocristaldo@gmail.com>
2025-11-12 18:50:04 -0300

    Se refactorizo WorldCell y se agrego Floor y Wall

--- src/main/java/org/thegoats/rolgar2/world/Floor.java
+++ src/main/java/org/thegoats/rolgar2/world/Floor.java
@@ -1,4 +1,8 @@
 package org.thegoats.rolgar2.world;
 
-public record Floor() {
+/**
+ * Representa el suelo de una celda
+ * @param isWalkable true si se puede caminar sobre, false en caso contrario
+ */
+public record Floor(boolean isWalkable) {
 }
--- src/main/java/org/thegoats/rolgar2/world/Wall.java
+++ src/main/java/org/thegoats/rolgar2/world/Wall.java
@@ -1,4 +1,8 @@
 package org.thegoats.rolgar2.world;
 
-public record Wall() {
+/**
+ * Representa un muro en una celda
+ * @param isClimbable true si se puede trepar, false en caso contrario
+ */
+public record Wall(boolean isClimbable) {
 }
