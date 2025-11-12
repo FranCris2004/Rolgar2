8c57a173e80931e44c8552e3720a7eddda107ce2
Franco Cristaldo <francocristaldo@gmail.com>
2025-11-12 19:25:19 -0300

    Se implemento CellConfig

--- src/main/java/org/thegoats/rolgar2/game/config/CellConfig.java
+++ src/main/java/org/thegoats/rolgar2/game/config/CellConfig.java
@@ -1,5 +1,7 @@
 package org.thegoats.rolgar2.game.config;
 
+import org.thegoats.rolgar2.util.Assert;
+
 /**
  * @param representation caracter que representa a la configuracion de celda en MapConfig.mapData
  * @param spritePath path a la imagen de la celda
@@ -7,4 +9,10 @@ package org.thegoats.rolgar2.game.config;
  * @param wallConfigName nombre de la configuracion del muro de la celda
  */
 public record CellConfig(Character representation, String spritePath, String floorConfigName, String wallConfigName) {
+    public CellConfig {
+        Assert.notNull(representation, "representation no puede ser nulo.");
+        Assert.notNull(spritePath, "spritePath no puede ser nulo.");
+        Assert.notNull(floorConfigName, "floorConfigName no puede ser nulo.");
+        Assert.notNull(wallConfigName, "wallConfigName no puede ser nulo.");
+    }
 }
