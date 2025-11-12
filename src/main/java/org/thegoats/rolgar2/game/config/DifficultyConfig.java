1a496ed006ee9af1bdef278e6f1a04a3d3ef571c
Franco Cristaldo <francocristaldo@gmail.com>
2025-11-12 19:22:27 -0300

    Se implemento FloorConfig

--- src/main/java/org/thegoats/rolgar2/game/config/DifficultyConfig.java
+++ src/main/java/org/thegoats/rolgar2/game/config/DifficultyConfig.java
@@ -1,8 +1,15 @@
 package org.thegoats.rolgar2.game.config;
 
+import org.thegoats.rolgar2.util.Assert;
+
 public record DifficultyConfig(
         String name,
         CharacterConfig playerConfig,
         CharacterConfig enemyConfig
 ) {
+    public DifficultyConfig {
+        Assert.notNull(name, "name no puede ser nulo");
+        Assert.notNull(playerConfig, "playerConfig no puede ser nulo");
+        Assert.notNull(enemyConfig, "enemyConfig no puede ser nulo");
+    }
 }
