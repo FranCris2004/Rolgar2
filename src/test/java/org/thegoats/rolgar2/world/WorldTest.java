001bd886262b42bced6d904093b6003a24ce9c7b
Franco Cristaldo <francocristaldo@gmail.com>
2025-11-12 22:02:28 -0300

    Se marco como TODO WorldTest

--- src/test/java/org/thegoats/rolgar2/world/WorldTest.java
+++ src/test/java/org/thegoats/rolgar2/world/WorldTest.java
@@ -8,7 +8,7 @@ public class WorldTest {
         World world = new World(3, 2, 4);
 
         world.forEach(worldCell -> {
-            worldCell.setFloor(new Floor(worldCell.hashCode() % 2 == 0));
+            // TODO
         });
 
         world.forEach(System.out::println);
