23d0df5866326bcef5ae0c3aae0b1c4e7451c1b6
Franco Cristaldo <francocristaldo@gmail.com>
2025-11-12 21:54:12 -0300

    Se agregaron constructores de Floor y Wall tomando sus respectivos config

--- src/main/java/org/thegoats/rolgar2/world/Wall.java
+++ src/main/java/org/thegoats/rolgar2/world/Wall.java
@@ -1,8 +1,18 @@
 package org.thegoats.rolgar2.world;
 
+import org.thegoats.rolgar2.game.config.WallConfig;
+import org.thegoats.rolgar2.util.Assert;
+
 /**
  * Representa un muro en una celda
  * @param isClimbable true si se puede trepar, false en caso contrario
  */
-public record Wall(boolean isClimbable) {
+public record Wall(WallConfig config, boolean isClimbable) {
+    public Wall {
+        Assert.notNull(config, "config no puede ser nulo");
+    }
+
+    public Wall(WallConfig config) {
+        this(config, config.isClimbable());
+    }
 }
