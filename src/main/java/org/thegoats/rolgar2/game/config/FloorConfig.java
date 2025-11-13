7f9c219c1c161a0036258ef38365255f76bc3eba
Franco Cristaldo <francocristaldo@gmail.com>
2025-11-12 21:45:57 -0300

    Se agrego FloorConfig.getBitmap

--- src/main/java/org/thegoats/rolgar2/game/config/FloorConfig.java
+++ src/main/java/org/thegoats/rolgar2/game/config/FloorConfig.java
@@ -1,8 +1,9 @@
 package org.thegoats.rolgar2.game.config;
 
 import org.thegoats.rolgar2.util.Assert;
+import org.thegoats.rolgar2.util.io.Bitmap;
 
-import java.io.File;
+import java.io.IOException;
 
 public record FloorConfig(String name, String spritePath, Boolean isWalkable) {
     public FloorConfig {
@@ -15,7 +16,7 @@ public record FloorConfig(String name, String spritePath, Boolean isWalkable) {
         }
     }
 
-    public File getSpriteFile() {
-        return new File(spritePath);
+    public Bitmap getBitmap() throws IOException {
+        return Bitmap.loadFromFile(spritePath);
     }
 }
