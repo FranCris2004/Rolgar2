6c224d87d17c2acf2707b2994004c073d1d49951
Franco Cristaldo <francocristaldo@gmail.com>
2025-11-14 19:37:13 -0300

    Se agrego name a Floor y se sobreescribio el equals para usarlo de identificador clave

--- src/main/java/org/thegoats/rolgar2/world/Floor.java
+++ src/main/java/org/thegoats/rolgar2/world/Floor.java
@@ -2,7 +2,12 @@ package org.thegoats.rolgar2.world;
 
 /**
  * Representa el suelo de una celda
+ * @param name nombre del bloque
  * @param isWalkable true si se puede caminar sobre, false en caso contrario
  */
-public record Floor(boolean isWalkable) {
+public record Floor(String name, boolean isWalkable) {
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof Floor && this.name.equals(((Floor) o).name);
+    }
 }
