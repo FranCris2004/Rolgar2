3aa27b4c5749cd3e2f9bc9cc2d07af57ee2bb9a6
Franco Cristaldo <francocristaldo@gmail.com>
2025-11-16 14:20:44 -0300

    Se implemento World y WorldViewer en Game

--- src/main/java/org/thegoats/rolgar2/game/Game.java
+++ src/main/java/org/thegoats/rolgar2/game/Game.java
@@ -1,8 +1,19 @@
 package org.thegoats.rolgar2.game;
 
+import org.thegoats.rolgar2.character.CharacterFactory;
 import org.thegoats.rolgar2.game.config.GameConfig;
+import org.thegoats.rolgar2.player.Player;
+import org.thegoats.rolgar2.util.Assert;
 import org.thegoats.rolgar2.util.Logger;
+import org.thegoats.rolgar2.util.io.Bitmap;
+import org.thegoats.rolgar2.world.World;
+import org.thegoats.rolgar2.world.WorldViewer;
 
+import java.awt.*;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 
 public final class Game {
@@ -10,11 +21,34 @@ public final class Game {
     public final Logger logger;
     public final GameConfig config;
     private final Set<GameCharacter> gameCharacters;
+    private final World world;
 
-    public Game(Logger logger, GameConfig config, Set<GameCharacter> gameCharacters) {
+    private final CharacterFactory playerCharacterFactory;
+    private final CharacterFactory enemyCharacterFactory;
+
+    public Game(GameConfig config, Logger logger, Random random, Set<Player> players) {
+        Assert.notNull(logger, "logger no puede ser nulo.");
+        Assert.notNull(config, "config no puede ser nulo.");
+        Assert.isTrue(players != null && !players.isEmpty(), "playerCharacterNames no puede ser nulo o vacio.");
+
         this.logger = logger;
         this.config = config;
-        this.gameCharacters = gameCharacters;
+        this.world = config.mapConfig().generateWorld();
+        this.playerCharacterFactory = new CharacterFactory(random, config.difficultyConfig().playerConfig());
+        this.enemyCharacterFactory = new CharacterFactory(random, config.difficultyConfig().enemyConfig());
+
+        this.gameCharacters = new HashSet<>();
+        for (Player player : players) {
+            this.gameCharacters.add(
+                    new GameCharacter(
+                            this,
+                            player,
+                            playerCharacterFactory.create(player.getName()),
+                            world.getRandomEmptyWalkableCell(random),
+                            GameCharacterPlayerTurnManager.class
+                    )
+            );
+        }
 
         logger.logDebug("Game constructor:");
         logger.logDebug("logger: " + logger);
@@ -29,9 +63,26 @@ public final class Game {
      * Iniciar el bucle de juego
      * Realizar las impresiones de fin del juego
      */
-    public void run()
-    {
+    public void run() throws IOException {
         logger.logDebug("Game run:");
+
+        var characterBitmap = new Bitmap(32, 32);
+        characterBitmap.fill(Color.magenta);
+
+        WorldViewer worldViewer = new WorldViewer(
+                config.mapConfig().getFloorBitmapMap(),
+                config.mapConfig().getWallBitmapMap(),
+                new HashMap<>(),
+                characterBitmap,
+                Color.darkGray,
+                new Color(0, 0, 0, 0),
+                new Color(0, 0, 0, 180),
+                100, 100
+        );
+
+        for (int layer = 0; layer < world.getLayerCount(); layer++) {
+            worldViewer.showLayers(world, 0, layer);
+        }
 
         while (hasNextTurn()) {
             nextTurn();
