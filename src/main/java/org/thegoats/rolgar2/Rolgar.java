7b9704e3aca85d6382be55b7bb0aa307370d6908
Franco Cristaldo <francocristaldo@gmail.com>
2025-11-16 14:21:40 -0300

    Se refactorizo la creacion de Game en Rolgar.main

--- src/main/java/org/thegoats/rolgar2/Rolgar.java
+++ src/main/java/org/thegoats/rolgar2/Rolgar.java
@@ -5,6 +5,8 @@ import org.thegoats.rolgar2.util.ConsoleLogger;
 import org.thegoats.rolgar2.util.LogLevel;
 import org.thegoats.rolgar2.util.Options;
 
+import java.io.IOException;
+
 public class Rolgar {
     public static void main(String[] args) {
         var logger = new ConsoleLogger(LogLevel.DEBUG);
@@ -23,16 +25,20 @@ public class Rolgar {
 
         boolean playAgain = true;
         while (playAgain) {
-            GameBuilder.createBuilder()
-                    // configura el juego
-                    .setLogger(logger)
-                    .selectDifficulty("./assets/difficulties/")
-                    .selectMap("./assets/maps/")
-                    .initPlayers()
-                    // construye el juego
-                    .build()
-                    // ejecuta el juego
-                    .run();
+            try {
+                GameBuilder.createBuilder()
+                        // configura el juego
+                        .setLogger(logger)
+                        .selectDifficulty("difficulties")
+                        .selectMap("maps")
+                        .initPlayers()
+                        // construye el juego
+                        .build()
+                        // ejecuta el juego
+                        .run();
+            } catch (IOException | RuntimeException e) {
+                throw new RuntimeException(e);
+            }
 
             playAgain = playAgainPrompt.choose().orElse("no").equals("si");
         }
