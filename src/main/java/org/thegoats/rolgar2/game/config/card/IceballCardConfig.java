140b9ad45b388fa5c10c5b4e15bb8c261f08f0f6
Franco Cristaldo <francocristaldo@gmail.com>
2025-11-17 14:15:28 -0300

    Se agrego IceballCardConfig

---
+++ src/main/java/org/thegoats/rolgar2/game/config/card/IceballCardConfig.java
@@ -0,0 +1,23 @@
+package org.thegoats.rolgar2.game.config.card;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.thegoats.rolgar2.card.Card;
+import org.thegoats.rolgar2.card.IceballCard;
+import org.thegoats.rolgar2.util.Assert;
+
+import java.util.Random;
+
+public record IceballCardConfig(
+        int durationFloor,
+        int durationRoof
+) implements CardConfig<IceballCard> {
+    public IceballCardConfig {
+        Assert.positive(durationFloor, "durationFloor");
+        Assert.isTrue(durationFloor > durationRoof, "durationRoof debe ser mayor a durationFloor");
+    }
+
+    @Override @JsonIgnore
+    public Card.Factory<IceballCard> getFactory(Random random) {
+        return new IceballCard.Factory(random, durationFloor, durationRoof);
+    }
+}
