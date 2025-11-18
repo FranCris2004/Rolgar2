1da9177784564654957822f06b5239b15381bd1f
Franco Cristaldo <francocristaldo@gmail.com>
2025-11-17 22:49:18 -0300

    Se agrego CardConfig

--- src/main/java/org/thegoats/rolgar2/game/config/card/CardConfigBundle.java
+++ src/main/java/org/thegoats/rolgar2/game/config/card/CardConfigBundle.java
@@ -1,6 +1,12 @@
 package org.thegoats.rolgar2.game.config.card;
 
+import org.thegoats.rolgar2.card.Card;
 import org.thegoats.rolgar2.util.Assert;
+import org.thegoats.rolgar2.util.structures.lists.LinkedList;
+
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
 public record CardConfigBundle(
         DoubleMoveCardConfig doubleMoveCardConfig,
@@ -23,5 +29,21 @@ public record CardConfigBundle(
         Assert.notNull(stealingCardConfig, "stealingCardConfig");
         Assert.notNull(strengthCardConfig, "strengthCardConfig");
         Assert.notNull(teleportCardConfig, "teleportCardConfig");
+    }
+
+    public Set<Card.Factory<? extends Card>> getFactories(Random random) {
+        var factories = new org.thegoats.rolgar2.util.structures.sets.Set<Card.Factory<? extends Card>>();
+
+        factories.add(doubleMoveCardConfig.getFactory(random));
+        factories.add(fireballCardConfig.getFactory(random));
+        factories.add(healingCardConfig.getFactory(random));
+        factories.add(iceballCardConfig.getFactory(random));
+        factories.add(invisibilityCardConfig.getFactory(random));
+        factories.add(shieldCardConfig.getFactory(random));
+        factories.add(stealingCardConfig.getFactory(random));
+        factories.add(strengthCardConfig.getFactory(random));
+        factories.add(teleportCardConfig.getFactory(random));
+
+        return factories;
     }
 }
--- src/main/java/org/thegoats/rolgar2/game/config/card/IceballCardConfig.java
+++ src/main/java/org/thegoats/rolgar2/game/config/card/IceballCardConfig.java
@@ -1,6 +1,5 @@
 package org.thegoats.rolgar2.game.config.card;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.thegoats.rolgar2.card.Card;
 import org.thegoats.rolgar2.card.IceballCard;
 import org.thegoats.rolgar2.util.Assert;
@@ -13,10 +12,10 @@ public record IceballCardConfig(
 ) implements CardConfig<IceballCard> {
     public IceballCardConfig {
         Assert.positive(durationFloor, "durationFloor");
-        Assert.isTrue(durationFloor > durationRoof, "durationRoof debe ser mayor a durationFloor");
+        Assert.isTrue(durationRoof > durationFloor, "durationRoof debe ser mayor a durationFloor");
     }
 
-    @Override @JsonIgnore
+    @Override
     public Card.Factory<IceballCard> getFactory(Random random) {
         return new IceballCard.Factory(random, durationFloor, durationRoof);
     }
