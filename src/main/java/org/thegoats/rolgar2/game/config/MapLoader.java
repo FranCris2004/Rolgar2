0aac6e36bbd48a59e4276d21f070713d177e0244
nahuel <nahuelestudio21@gmail.com>
2025-11-18 05:56:08 -0300

    Se agregaron comentarios a MapLoader

--- src/main/java/org/thegoats/rolgar2/game/config/MapLoader.java
+++ src/main/java/org/thegoats/rolgar2/game/config/MapLoader.java
@@ -15,10 +15,24 @@ public final class MapLoader {
 
     private MapLoader() {}
 
+    /**
+     * Carga todos los mapas (MapConfig) ubicados en el directorio indicado.
+     * @param path nombre o ruta del directorio de recursos donde se encuentran los JSON de mapas
+     * @return un conjunto de objetos MapConfig cargados desde ese directorio
+     * @throws IOException si ocurre un error de lectura de archivos
+     * @throws URISyntaxException si hay problemas al convertir la URL del recurso a URI
+     */
     public static Set<MapConfig> loadMaps(String path) throws IOException, URISyntaxException {
         return loadFromResourceDirectory(path);
     }
 
+    /**
+     * Carga todos los MapConfig que se encuentran dentro de un directorio de recursos
+     * @param resourceDirName  nombre del directorio de recursos
+     * @return un conjunto de objetos MapConfig leídos desde los archivos JSON del directorio
+     * @throws IOException  si ocurre un error de lectura de archivos
+     * @throws URISyntaxException si hay problemas al resolver la ubicación del recurso
+     */
     private static Set<MapConfig> loadFromResourceDirectory(String resourceDirName) throws IOException, URISyntaxException {
         Set<MapConfig> configs = new HashSet<>();
 
@@ -57,10 +71,22 @@ public final class MapLoader {
         return configs;
     }
 
+    /**
+     * Carga un único MapConfig desde el archivo JSON indicado por la ruta.
+     * @param path ruta del archivo JSON que contiene la configuración del mapa
+     * @return un objeto MapConfig generado a partir del contenido del archivo
+     * @throws IOException si ocurre un error de lectura o el JSON es inválido
+     */
     private static MapConfig loadOne(Path path) throws IOException {
         return mapper.readValue(path.toFile(), MapConfig.class);
     }
 
+    /**
+     * Devuelve un FileSystem asociado al JAR desde el que se está ejecutando la clase MapLoader.
+     * @return un FileSystem que representa el JAR actual
+     * @throws IOException si ocurre un error al abrir o crear el FileSystem
+     * @throws URISyntaxException si hay problemas al obtener la URI del JAR
+     */
     private static FileSystem getJarFileSystem() throws IOException, URISyntaxException {
         try {
             return FileSystems.getFileSystem(MapLoader.class.getProtectionDomain()
