cf0de4345b18516c6826511818ab4938b90ae4be
Franco Cristaldo <francocristaldo@gmail.com>
2025-11-17 22:49:05 -0300

    Se agrego CardConfig

--- src/main/java/org/thegoats/rolgar2/game/config/card/CardConfig.java
+++ src/main/java/org/thegoats/rolgar2/game/config/card/CardConfig.java
@@ -1,9 +1,11 @@
 package org.thegoats.rolgar2.game.config.card;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.thegoats.rolgar2.card.Card;
 
 import java.util.Random;
 
 public interface CardConfig<T extends Card> {
+    @JsonIgnore
     Card.Factory<T> getFactory(Random random);
 }
