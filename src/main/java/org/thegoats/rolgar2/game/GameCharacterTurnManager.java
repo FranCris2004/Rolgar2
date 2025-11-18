6fe25ad141b674b879d810a2fd096186db9f2dba
nahuel <nahuelestudio21@gmail.com>
2025-11-18 18:32:09 -0300

    Se agregaron comentarios a GameCharacterTurnManager

--- src/main/java/org/thegoats/rolgar2/game/GameCharacterTurnManager.java
+++ src/main/java/org/thegoats/rolgar2/game/GameCharacterTurnManager.java
@@ -7,11 +7,18 @@ public abstract class GameCharacterTurnManager {
     protected final GameCharacter gameCharacter;
     protected final Logger logger;
 
+    /**
+     * Construye un administrador de turnos para el personaje dado.
+     * @param gameCharacter personaje del juego, no debe ser nulo
+     */
     public GameCharacterTurnManager(GameCharacter gameCharacter) {
         Assert.notNull(gameCharacter, "gameCharacter no puede ser nulo.");
         this.gameCharacter = gameCharacter;
         this.logger = gameCharacter.getGame().logger;
     }
 
+    /**
+     * Ejecuta el turno completo del personaje.
+     */
     public abstract void doTurn();
 }
